1114dee3bb15c5964c09b8f22b8ec2aa5f0b2bf8
D7703_16 <d7703_16@D7703-16ui-iMac.local>
2018-04-12 11:18:57 +0900

    stop watch

--- stopwatch/ViewController.swift
+++ stopwatch/ViewController.swift
@@ -10,16 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var timeLabel: UILabel!
+    var count = 0
+    var myTrimer = Timer()
+
+    @IBOutlet weak var start: UIButton!
+    @IBOutlet weak var stop: UIButton!
+    @IBOutlet weak var reset: UIButton!
+    
+    func updateTime() {
+        count = count + 1
+        let min = count / 60 / 100
+        let sec = (count - (min * 60 * 100)) / 100
+        let msec = count - (min * 60 * 100) - (sec * 100)
+        timeLabel.text = String(format :  "%02d:%02d:%02d", min, sec, msec)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func start(_ sender: Any) {
+        myTrimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer) in
+            self.updateTime()
+        })
+        changeButtonState(startS : false, stopS : true, resetS : true)
     }
-
+    
+    @IBAction func stop(_ sender: Any) {
+        myTrimer.invalidate()
+        changeButtonState(startS : true, stopS : false, resetS : true)
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        myTrimer.invalidate()
+        count = 0
+        timeLabel.text = "00:00:00"
+        changeButtonState(startS : true, stopS : true, resetS : false)
+        
+    }
+    
+    func changeButtonState(startS : Bool, stopS : Bool, resetS : Bool) {
+        start.isEnabled = startS
+        stop.isEnabled = stopS
+        reset.isEnabled = resetS
+    }
 
 }
 
